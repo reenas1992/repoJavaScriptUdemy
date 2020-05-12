c44527c15b8d2533c6f7f35d6b55aca471b86bbb
reenas1992 <reena.y.jambhulkar@gmail.com>
2020-05-13 02:06:57 +0530

    Ternary Operator and Switch Statements

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -1,111 +1,119 @@
 // console.log("Hello World!!!");
 
-// /* Variable and data types */
-// var strFirstName = "Reena";
-// console.log(strFirstName);
+/* Variable and data types */
+/*
+var strFirstName = "Reena";
+console.log(strFirstName);
 
-// var strLastName = "Sakhare";
-// console.log(strLastName);
+var strLastName = "Sakhare";
+console.log(strLastName);
 
-// var strFullName;
-// console.log("FullName: " + strFirstName + " " + strLastName);
+var strFullName;
+console.log("FullName: " + strFirstName + " " + strLastName);
 
-// var intAge = 27;
-// var boolFullAge = true;
-// console.log(boolFullAge);
+var intAge = 27;
+var boolFullAge = true;
+console.log(boolFullAge);
 
-// var strJob;
-// console.log(strJob);
+var strJob;
+console.log(strJob);
 
-// var strJob = "SoftWare Developer";
-// console.log(strJob);
+var strJob = "SoftWare Developer";
+console.log(strJob);
 
-// // Variable Naming Rule
-// var _2years = 2;
-// var johnMark = "Jhon and Mark";
+// Variable Naming Rule
+var _2years = 2;
+var johnMark = "Jhon and Mark";
+*/
 
-// /*** Variable Mutation And Type Coercion ***/
-// var boolIsMarried = true;
-// console.log(
-//   strFirstName +
-//     " is a " +
-//     intAge +
-//     " year old " +
-//     strJob +
-//     " is she married? " +
-//     boolIsMarried
-// );
+/*** Variable Mutation And Type Coercion ***/
+/*
+var boolIsMarried = true;
+console.log(
+  strFirstName +
+    " is a " +
+    intAge +
+    " year old " +
+    strJob +
+    " is she married? " +
+    boolIsMarried
+);
 
-// // Variable Mutation
-// strAge = "Twenty Seven";
-// strJob = "Teacher";
-// alert(
-//   strFirstName +
-//     " is a " +
-//     intAge +
-//     " year old " +
-//     strJob +
-//     " is she married " +
-//     boolIsMarried
-// );
+// Variable Mutation
+strAge = "Twenty Seven";
+strJob = "Teacher";
+alert(
+  strFirstName +
+    " is a " +
+    intAge +
+    " year old " +
+    strJob +
+    " is she married " +
+    boolIsMarried
+);
 
-// var strLastName = prompt("What is her Last Name?");
-// console.log(strFirstName + " " + strLastName);
+var strLastName = prompt("What is her Last Name?");
+console.log(strFirstName + " " + strLastName);
+*/
 
 /*** Basic Operators***/
-// var intYear, yearReena, yearSameer;
-// now = 2020;
-// intAgeReena = 28;
-// intAgeSameer = 31;
+/*
+var intYear, yearReena, yearSameer;
+now = 2020;
+intAgeReena = 28;
+intAgeSameer = 31;
 
 // // Maths Operations
-// yearReena = now - intAgeReena;
-// yearSameer = now - intAgeSameer;
-// console.log(yearReena);
-// console.log(yearSameer);
+yearReena = now - intAgeReena;
+yearSameer = now - intAgeSameer;
+console.log(yearReena);
+console.log(yearSameer);
 
-// console.log(now + 3);
-// console.log(now * 3);
-// console.log(now / 3);
-// console.log(now - 3);
+console.log(now + 3);
+console.log(now * 3);
+console.log(now / 3);
+console.log(now - 3);
 
-// // Logical Operator
-// var boolAgeOlder = intAgeReena < intAgeSameer;
-// console.log(boolAgeOlder);
+// Logical Operator
+var boolAgeOlder = intAgeReena < intAgeSameer;
+console.log(boolAgeOlder);
 
-// //// Type of operator
-// console.log(typeof boolAgeOlder);
-// console.log(typeof intAgeReena);
-// console.log(typeof "Sameer is older tha Reena");
-// var x;
-// console.log(typeof x);
+// Type of operator
+console.log(typeof boolAgeOlder);
+console.log(typeof intAgeReena);
+console.log(typeof "Sameer is older tha Reena");
+var x;
+console.log(typeof x);
+*/
 
 /*** Operator Precedence ***/
-// var intNow = 2020;
-// var intYearReena = 1992;
-// var intFullAge = 20;
+/*
+var intNow = 2020;
+var intYearReena = 1992;
+var intFullAge = 20;
 
 // Multiple Operator
-// var boolIsFullAge = intNow - intYearReena >= intFullAge; //true
-// console.log(boolIsFullAge);
+var boolIsFullAge = intNow - intYearReena >= intFullAge; //true
+console.log(boolIsFullAge);
 
 //Grouping
-// var intAgeReena = intNow - intYearReena;
-// var intAgeSameer = 32;
-// var fAverage = (intAgeReena + intAgeSameer) / 2;
-// console.log(fAverage);
+var intAgeReena = intNow - intYearReena;
+var intAgeSameer = 32;
+var fAverage = (intAgeReena + intAgeSameer) / 2;
+console.log(fAverage);
 
 //Multiple Assignments
-// var x, y;
-// x = y = (5 + 4) * 3 - 7; // 4*7-5//49-4// 20
+var x, y;
+x = y = (5 + 4) * 3 - 7; // 4*7-5//49-4// 20
 
 //More Operator
-// x *= 2;
-// console.log(x);
-// x += 20;
-// console.log(x);
-// x--;
-// console.log(x);
+x *= 2;
+console.log(x);
+x += 20;
+console.log(x);
+x--;
+console.log(x);
+*/
 
 /*****************************
  * CODING CHALLENGE 1
@@ -137,38 +145,41 @@ console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
 */
 
 /*** If / else Statement ***/
-// var strFirstName = "Jhone";
-// var strCivilStatus = "Single";
+/*
+var strFirstName = "Jhone";
+var strCivilStatus = "Single";
 
-// if (strCivilStatus == "married") {
-//   console.log(strFirstName + " is married!");
-// } else {
-//   console.log(strFirstName + " will hopefully marry soon : ");
-// }
+if (strCivilStatus == "married") {
+  console.log(strFirstName + " is married!");
+} else {
+  console.log(strFirstName + " will hopefully marry soon : ");
+}
 
-// var boolIsMarried = true;
-// if (boolIsMarried) {
-//   console.log(strFirstName + " is married! ");
-// } else {
-//   console.log(strFirstName + " will hopefully marry soon : ");
-// }
+var boolIsMarried = true;
+if (boolIsMarried) {
+  console.log(strFirstName + " is married! ");
+} else {
+  console.log(strFirstName + " will hopefully marry soon : ");
+}
 
-// var massMark = 80; // kg
-// var heightMark = 1.79; //meters
+var massMark = 80; // kg
+var heightMark = 1.79; //meters
 
-// var massJohn = 89;
-// var heightJohn = 1.92;
+var massJohn = 89;
+var heightJohn = 1.92;
 
-// var BMIMark = massMark / (heightMark * heightMark);
-// var BMIJohn = massJohn / (heightMark * heightMark);
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightMark * heightMark);
 
-// if (BMIMark > BMIJohn) {
-//   console.log("Mark's BMI is highter than Jhon's. ");
-// } else {
-//   console.log("Jhon's BMI is highter than Mark's. ");
-// }
+if (BMIMark > BMIJohn) {
+  console.log("Mark's BMI is highter than Jhon's. ");
+} else {
+  console.log("Jhon's BMI is highter than Mark's. ");
+}
+*/
 
 /*** Boolean Logic ***/
+/*
 var strFirstName = "Reena";
 var intAge = 27;
 
@@ -178,4 +189,47 @@ if (intAge < 20) {
   console.log(strFirstName + "is a young girl. ");
 } else if (intAge > 25 && intAge < 30) {
   console.log(strFirstName + " is a woman. ");
+}
+*/
+
+/*** The Ternary Operator and Switch Statements ***/
+var strFirstName = "Reena";
+var intAge = 27;
+
+// Ternary Operator
+intAge >= 18
+  ? console.log(strFirstName + " drink Beer. ")
+  : console.log(strFirstName + " drink Juice. ");
+var drink = intAge >= 18 ? "Beer" : "Juice";
+console.log(drink);
+
+// Switch Statements
+var strJob = "Designer";
+switch (strJob) {
+  case "Doctor":
+    console.log(strFirstName + " take care of patient. ");
+    break;
+  case "Teacher":
+    console.log(strFirstName + " teaches kids how to code. ");
+    break;
+  case "Designer":
+    console.log(strFirstName + " designs beautifull website. ");
+    break;
+  default:
+    console.log(strFirstName + " does something else. ");
+}
+
+intAge = 56;
+switch (true) {
+  case intAge < 13:
+    console.log(strFirstName + " is a girl. ");
+    break;
+  case intAge >= 13 && intAge < 30:
+    console.log(strFirstName + " is a teenager. ");
+    break;
+  case intAge >= 30 && intAge < 40:
+    console.log(strFirstName + "is a young girl. ");
+    break;
+  default:
+    console.log(strFirstName + " is a woman. ");
 }
