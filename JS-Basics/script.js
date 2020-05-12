ffc302b7faaddcde8ca1d3f2227b7562ecbe6999
reenas1992 <reena.y.jambhulkar@gmail.com>
2020-05-12 14:30:06 +0530

    1. Variable and Data type 2. Variable Mutation and type Coercion 3. Basic Operator

---
+++ JS-Basics/script.js
@@ -0,0 +1,81 @@
+// console.log("Hello World!!!");
+
+// /* Variable and data types */
+// var strFirstName = "Reena";
+// console.log(strFirstName);
+
+// var strLastName = "Sakhare";
+// console.log(strLastName);
+
+// var strFullName;
+// console.log("FullName: " + strFirstName + " " + strLastName);
+
+// var intAge = 27;
+// var boolFullAge = true;
+// console.log(boolFullAge);
+
+// var strJob;
+// console.log(strJob);
+
+// var strJob = "SoftWare Developer";
+// console.log(strJob);
+
+// // Variable Naming Rule
+// var _2years = 2;
+// var johnMark = "Jhon and Mark";
+
+// /*** Variable Mutation And Type Coercion ***/
+// var boolIsMarried = true;
+// console.log(
+//   strFirstName +
+//     " is a " +
+//     intAge +
+//     " year old " +
+//     strJob +
+//     " is she married? " +
+//     boolIsMarried
+// );
+
+// // Variable Mutation
+// strAge = "Twenty Seven";
+// strJob = "Teacher";
+// alert(
+//   strFirstName +
+//     " is a " +
+//     intAge +
+//     " year old " +
+//     strJob +
+//     " is she married " +
+//     boolIsMarried
+// );
+
+// var strLastName = prompt("What is her Last Name?");
+// console.log(strFirstName + " " + strLastName);
+
+/*** Basic Operators***/
+var intYear, yearReena, yearSameer;
+now = 2020;
+intAgeReena = 28;
+intAgeSameer = 31;
+
+// Maths Operations
+yearReena = now - intAgeReena;
+yearSameer = now - intAgeSameer;
+console.log(yearReena);
+console.log(yearSameer);
+
+console.log(now + 3);
+console.log(now * 3);
+console.log(now / 3);
+console.log(now - 3);
+
+// Logical Operator
+var boolAgeOlder = intAgeReena < intAgeSameer;
+console.log(boolAgeOlder);
+
+// Type of operator
+console.log(typeof boolAgeOlder);
+console.log(typeof intAgeReena);
+console.log(typeof "Sameer is older tha Reena");
+var x;
+console.log(typeof x);
